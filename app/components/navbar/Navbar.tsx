2268337117ccfc1c6f840bba8f3c7a21ef2fa15e
Laura <lauracontreras19102003@gmail.com>
2025-02-26 12:44:24 -0600

    03 Categories on the front page

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -8,7 +8,7 @@ import AddPropertyButton from './AddPropertyButton';
 const Navbar =() => {
     return (
         <nav className="w-full fixed top-0 left-0 py-6 border-bg-white z-10">
-            <div className="w-full-[1500px] mx-auto px-6">
+            <div className="max-w-full-[1500px] mx-auto px-6">
                 <div className="flex justify-between items-center">
                     <Link href="/">
                         <Image
