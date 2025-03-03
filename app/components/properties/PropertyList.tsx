dcae45504eaf0e9e45b267e780a02a4a4c3fee22
Laura <lauracontreras19102003@gmail.com>
2025-03-03 10:00:15 -0600

    list properties on the front page (static)

---
+++ app/components/properties/PropertyList.tsx
@@ -0,0 +1,12 @@
+import PropertyListItem from "./PropertyListitem";
+const PropertyList = () => {
+    return (
+        <>
+        <PropertyListItem/>
+        <PropertyListItem/>
+        <PropertyListItem/>
+        </>
+    )
+}
+
+export default PropertyList;
---
+++ app/properties/ReservationSidebar.tsx
@@ -0,0 +1,9 @@
+const ReservationSidebar = () => {
+    return (
+        <aside className="p-6 col-span-2  rounded-xl border-gray-300 shadow-xl">
+            hagsvaj
+        </aside>
+    )
+}
+
+export default ReservationSidebar;
