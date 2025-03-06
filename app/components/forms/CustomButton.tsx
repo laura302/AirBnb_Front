8658760607fb4acc8aa6dfbc6527663a0d65ec68
Laura <lauracontreras19102003@gmail.com>
2025-03-06 10:49:46 -0600

    10 Detail page for chat (static)

---
+++ app/components/forms/CustomButton.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+interface CustomButtonProps {
+    label: string;
+    className?: string;
+    onClick: () => void;
+}
+
+const CustomButton: React.FC<CustomButtonProps> = ({
+    label,
+    className,
+    onClick
+}) =>  {
+    return (
+        <div 
+            onClick={onClick}
+            className={`w-full py-4 bg-airbnb hover:bg-airbnb-Dark text-white text-center rounded-xl transition cursor-pointer ${className}`}
+        >
+            {label}
+        </div>
+    )
+}
+
+export default CustomButton;
