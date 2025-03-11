cb5e03a5b3685a65256abd766fd5ddc66b1f9ad0
Laura <lauracontreras19102003@gmail.com>
2025-03-10 21:15:59 -0600

    13 Create reusable model window

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import {  Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
+import Modal from "./components/modals/Modal";
 
 const inter = Inter({subsets: ["latin"]});
 
@@ -15,6 +16,10 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const content = (
+      <p>Yo yo</p>
+    )
+  
   return (
     <html lang="en">
       <body className={inter.className}>
