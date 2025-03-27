930f4cd0ed5621172ebf821cbcd7b7b73220819e
Laura <lauracontreras19102003@gmail.com>
2025-03-27 09:31:48 -0600

    Log out

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,24 +3,37 @@
 import { cookies } from "next/headers";
 
 export async function handleLogin(userId:string, accessToken: string, refreshToken: string) {
-    cookies().set('session_userid', userId, {
+    (await cookies()).set('session_userid', userId, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
         maxAge: 60 * 60 * 24 * 7, //One week
         path: '/'
     });
 
-    cookies().set('session_access_token', accessToken , {
+    (await cookies()).set('session_access_token', accessToken , {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
         maxAge: 60 * 60,  //60 minutes
         path: '/'
     });
 
-    cookies().set('session_refresh_token', refreshToken , {
+    (await cookies()).set('session_refresh_token', refreshToken , {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
         maxAge: 60 * 60 * 24 * 7,  // one week        path: '/'
     });
-    
+}
+
+export async function resetAuthCookies() {
+    (await cookies()).set('session_userid', '');
+    (await cookies()).set('session_access_token', '');
+    (await cookies()).set('session_refresh_token', '');
+}
+
+//
+// Get data
+
+export async function getUserId() {
+    const userId = (await cookies()).get('session_userid')?.value
+    return userId ? userId : null
 }
